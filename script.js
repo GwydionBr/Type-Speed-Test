a26e54bbf7e3f3882411e8d20707bbdb658c4aba
GwydionBr <155477872+GwydionBr@users.noreply.github.com>
2024-05-15 21:53:50 +0200

    Appereance of the own Text input

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 document.getElementById("example").onclick =
     function(){
-        document.getElementById("demo").innerHTML =
+        document.getElementById("text").innerHTML =
             "Example"
     }
 
 document.getElementById("own_text").onclick =
     function(){
-        document.getElementById("demo").innerHTML =
-            "Own Text"
+        document.getElementById("text").innerHTML =
+            `<fieldset class="column-flex">
+                <legend>Enter your Text:</legend>
+                <textarea style='width: 70vw'></textarea>
+                <br>
+                <button type="button">Submit</button>
+             </fieldset>
+            `
+
+    }
+
+document.getElementById("introduction").onclick =
+    function (){
+        document.getElementById("reset").innerHTML =
+            "<button>Reset Site</button>"
     }
