da4155205dc3392d337663e7c8aa85560820ed94
GwydionBr <155477872+GwydionBr@users.noreply.github.com>
2024-05-15 20:42:21 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("example").onclick =
+    function(){
+        document.getElementById("demo").innerHTML =
+            "Example"
+    }
+
+document.getElementById("own_text").onclick =
+    function(){
+        document.getElementById("demo").innerHTML =
+            "Own Text"
+    }
