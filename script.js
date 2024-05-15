30d283a1789742f4efdb8705b2f771ab652082a2
GwydionBr <155477872+GwydionBr@users.noreply.github.com>
2024-05-15 22:29:44 +0200

    Choice for Pre formulated Text added

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 document.getElementById("example").onclick =
     function(){
         document.getElementById("text").innerHTML =
-            "Example"
+            `
+            <fieldset class="column-flex">
+                <legend>How many characters should the text include?</legend>
+                <input type="radio" id="char_60" name="chars">
+                <label for="char_60">60 Characters</label>
+                <br>
+                <input type="radio" id="char_120" name="chars">
+                <label for="char_120">120 Characters</label>
+                <br>
+                <input type="radio" id="char_240" name="chars">
+                <label for="char_240">240 Characters</label>
+                <br>
+                <input type="radio" id="char_480" name="chars">
+                <label for="char_480">480 Characters</label>
+                <br>
+                <button type="button" onclick="getTypingArea()">Submit</button>
+            </fieldset>
+            `
     }
 
 document.getElementById("own_text").onclick =
     function(){
         document.getElementById("text").innerHTML =
-            `<fieldset class="column-flex">
+            `
+            <fieldset class="column-flex">
                 <legend>Enter your Text:</legend>
                 <textarea style='width: 70vw'></textarea>
                 <br>
-                <button type="button">Submit</button>
+                <button type="button" onclick="getTypingArea()">Submit</button>
              </fieldset>
             `
-
     }
 
 document.getElementById("introduction").onclick =
     function (){
         document.getElementById("reset").innerHTML =
             "<button>Reset Site</button>"
+    }
+
+function getTypingArea(){
+        document.getElementById("typing-area").innerHTML =
+            `<label for="typingField">Start Typing here:</label>
+            <textarea id="typingField"></textarea>
+            `
     }
